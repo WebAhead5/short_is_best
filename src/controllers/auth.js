884ed8b01a0c4094ed987a56701019dfe6cb6ee9
fridabu <f0504070002@gmail.com>
2020-05-06 20:21:03 +0300

    adding hardcoded example of posts

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -69,7 +69,36 @@ exports.authenticate = async (req, res) => {
           }
           console.log(token)
           res.cookie('access_token', token);
-          res.redirect('/');
+          
+          const tweet = [{
+            postid: 1,
+            name: 'James',
+            handle: '@jokerjames',
+            img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+            content: "If you don't succeed, dust yourself off and try again.",
+            likes: 10,
+            postdate: "20-01-2010"
+
+          },
+          {
+
+            postid: 2,
+            name: 'Fatima',
+            handle: '@fantasticfatima',
+            img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+            tweet: 'Better late than never but never late is better.',
+            likes: 12,
+          },
+          {
+            id: 3,
+            name: 'Xin',
+            handle: '@xeroxin',
+            img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+            tweet: 'Beauty in the struggle, ugliness in the success.',
+            likes: 18,
+          }
+          ]
+          res.render('home', { signedIn: true, tweet: tweet });
         });
 
       } else {
